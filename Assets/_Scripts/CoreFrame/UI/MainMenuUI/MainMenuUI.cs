ee2a942614b440a59512d680821d914dd6e6deec
michael811125 <michael811125@gmail.com>
2024-01-31 16:57:53 +0800

    fixed method name

--- Assets/_Scripts/CoreFrame/UI/MainMenuUI/MainMenuUI.cs
+++ Assets/_Scripts/CoreFrame/UI/MainMenuUI/MainMenuUI.cs
@@ -67,14 +67,14 @@ public class MainMenuUI : UIBase
     {
     }
 
-    protected override void ShowAnime(AnimeEndCb animeEndCb)
+    protected override void ShowAnimation(AnimationEnd animationEnd)
     {
-        animeEndCb(); // Must Keep, Because Parent Already Set AnimCallback
+        animationEnd(); // Must call if animation end
     }
 
-    protected override void HideAnime(AnimeEndCb animeEndCb)
+    protected override void HideAnimation(AnimationEnd animationEnd)
     {
-        animeEndCb(); // Must Keep, Because Parent Already Set AnimCallback
+        animationEnd(); // Must call if animation end
     }
 
     protected override void OnClose()
