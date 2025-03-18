e88fbecf1a3e8c68a51a9c57026eed65226b00a2
michael811125 <michael811125@gmail.com>
2025-03-18 15:49:44 +0800

    fixed namespace

--- Assets/_Scripts/Main/Utilities/Gizmo/Scripts/Runtime/PointGizmo.cs
+++ Assets/_Scripts/Main/Utilities/Gizmo/Scripts/Runtime/PointGizmo.cs
@@ -1,55 +1,58 @@
 using UnityEngine;
 
-public class PointGizmo : MonoBehaviour
+namespace FlappyBird.Main.Runtime
 {
-    enum DrawType
+    public class PointGizmo : MonoBehaviour
     {
-        Solid,
-        Wire
-    }
-
-    enum ColorSet
-    {
-        Red,
-        Yellow,
-        Green,
-        Cyan,
-        Blue
-    }
-
-    [SerializeReference] private ColorSet _colorSet = ColorSet.Red;
-    [SerializeField] private DrawType _drawType = DrawType.Solid;
-    public float radius = 0.15f;
-
-    private void OnDrawGizmos()
-    {
-        switch (this._colorSet)
+        enum DrawType
         {
-            case ColorSet.Red:
-                Gizmos.color = Color.red;
-                break;
-            case ColorSet.Yellow:
-                Gizmos.color = Color.yellow;
-                break;
-            case ColorSet.Green:
-                Gizmos.color = Color.green;
-                break;
-            case ColorSet.Cyan:
-                Gizmos.color = Color.cyan;
-                break;
-            case ColorSet.Blue:
-                Gizmos.color = Color.blue;
-                break;
+            Solid,
+            Wire
         }
 
-        switch (this._drawType)
+        enum ColorSet
         {
-            case DrawType.Solid:
-                Gizmos.DrawSphere(this.transform.position, this.radius);
-                break;
-            case DrawType.Wire:
-                Gizmos.DrawWireSphere(this.transform.position, this.radius);
-                break;
+            Red,
+            Yellow,
+            Green,
+            Cyan,
+            Blue
+        }
+
+        [SerializeReference] private ColorSet _colorSet = ColorSet.Red;
+        [SerializeField] private DrawType _drawType = DrawType.Solid;
+        public float radius = 0.15f;
+
+        private void OnDrawGizmos()
+        {
+            switch (this._colorSet)
+            {
+                case ColorSet.Red:
+                    Gizmos.color = Color.red;
+                    break;
+                case ColorSet.Yellow:
+                    Gizmos.color = Color.yellow;
+                    break;
+                case ColorSet.Green:
+                    Gizmos.color = Color.green;
+                    break;
+                case ColorSet.Cyan:
+                    Gizmos.color = Color.cyan;
+                    break;
+                case ColorSet.Blue:
+                    Gizmos.color = Color.blue;
+                    break;
+            }
+
+            switch (this._drawType)
+            {
+                case DrawType.Solid:
+                    Gizmos.DrawSphere(this.transform.position, this.radius);
+                    break;
+                case DrawType.Wire:
+                    Gizmos.DrawWireSphere(this.transform.position, this.radius);
+                    break;
+            }
         }
     }
 }
