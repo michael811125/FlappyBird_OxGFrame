fc1bb42212d48a3fd9c36c959ccf94707a1701ad
michael811125 <michael811125@gmail.com>
2025-03-18 15:49:44 +0800

    fixed namespace

--- Assets/_Scripts/Main/Utilities/Gizmo/Scripts/Runtime/RectGizmo.cs
+++ Assets/_Scripts/Main/Utilities/Gizmo/Scripts/Runtime/RectGizmo.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
 
-public class RectGizmo : MonoBehaviour
+namespace FlappyBird.Main.Runtime
 {
-    public Color color = new Color32(0, 255, 255, 255);
-
-    public Vector2 originPos = new Vector2(0f, 0f);
-    public Vector2 rectSize = new Vector2(0f, 0f);
-
-    private void OnDrawGizmos()
+    public class RectGizmo : MonoBehaviour
     {
-        Gizmos.color = this.color;
-        Gizmos.DrawLine(new Vector3(this.originPos.x, this.originPos.y, 0), new Vector3(this.originPos.x, this.rectSize.y, 0));  // A點 -> B點
-        Gizmos.DrawLine(new Vector3(this.originPos.x, this.rectSize.y, 0), new Vector3(this.rectSize.x, this.rectSize.y, 0));    // B點 -> C點  
-        Gizmos.DrawLine(new Vector3(this.rectSize.x, this.rectSize.y, 0), new Vector3(this.rectSize.x, this.originPos.y, 0));    // C點 -> D點
-        Gizmos.DrawLine(new Vector3(this.rectSize.x, this.originPos.y, 0), new Vector3(this.originPos.x, this.originPos.y, 0));  // D點 -> A點
+        public Color color = new Color32(0, 255, 255, 255);
+
+        public Vector2 originPos = new Vector2(0f, 0f);
+        public Vector2 rectSize = new Vector2(0f, 0f);
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = this.color;
+            Gizmos.DrawLine(new Vector3(this.originPos.x, this.originPos.y, 0), new Vector3(this.originPos.x, this.rectSize.y, 0));  // A點 -> B點
+            Gizmos.DrawLine(new Vector3(this.originPos.x, this.rectSize.y, 0), new Vector3(this.rectSize.x, this.rectSize.y, 0));    // B點 -> C點  
+            Gizmos.DrawLine(new Vector3(this.rectSize.x, this.rectSize.y, 0), new Vector3(this.rectSize.x, this.originPos.y, 0));    // C點 -> D點
+            Gizmos.DrawLine(new Vector3(this.rectSize.x, this.originPos.y, 0), new Vector3(this.originPos.x, this.originPos.y, 0));  // D點 -> A點
+        }
     }
 }
